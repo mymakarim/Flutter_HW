5e93dd3226b3a34d721e9f1c322409c59ec116ec
ymakarim <ymakarim@gmail.com>
2021-01-14 11:02:16 +0430

    Initial commit

---
+++ android/app/src/main/kotlin/com/codenawis/hello_world/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codenawis.hello_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/task1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Task1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Task 1"),
+      ),
+      body: Center(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+}
